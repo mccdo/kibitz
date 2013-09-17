dbbf245628bb90c721925e57dce48e65717133d4
John Murphy <murphybytes@gmail.com>
2013-09-16 23:07:38 -0500

    added ability for workers to publish status messages

---
+++ src/kibitz/messages/worker_status_message.cpp
@@ -0,0 +1,41 @@
+#include <kibitz/messages/worker_status_message.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+using namespace boost::posix_time;
+namespace kibitz {
+  
+  const char* worker_status_message::NOTIFICATION_TYPE = "worker_status_message";
+
+  worker_status_message::worker_status_message( const string& worker_type, const string& worker_id, worker_status_t status ) 
+    :notification_message( worker_status_message::NOTIFICATION_TYPE ),
+     worker_type_( worker_type ),
+     worker_id_(worker_id),
+     status_(status),
+     timestamp_( to_iso_extended_string( microsec_clock::universal_time() ) )  {
+  }
+
+  worker_status_message::worker_status_message( JSON::Object::Ptr json ) 
+    :notification_message( json ) {
+    get_value( json, "worker_type", worker_type_ );
+    get_value( json, "worker_id", worker_id_ );
+    int status;
+    get_value( json, "status", status );
+    status_ = static_cast<worker_status_t>(status);
+    get_value( json, "timestamp", timestamp_ );
+  }
+
+  string worker_status_message::to_json() const {
+    stringstream stm;
+    JSON::Object::Ptr json;
+    read_json( "{}", json );
+    notification_message::populate_header( json ) ;
+    json->set( "worker_type", worker_type_ );
+    json->set( "worker_id", worker_id_ );
+    json->set( "status", static_cast<int>(status_) );
+    json->set( "timestamp", timestamp_ ) ;
+    json->stringify( stm );
+    return stm.str() ;
+  }
+  
+
+}
---
+++ src/kibitz/messages/worker_status_message.hpp
@@ -0,0 +1,39 @@
+#ifndef worker_status_message_hpp
+#define worker_status_message_hpp
+
+#include <kibitz/messages/notification_message.hpp>
+
+namespace kibitz {
+
+  enum worker_status_t {
+    START = 0,
+    STOP = 1,
+    WORK_RECIEVED = 2,
+    WORK_SENT = 3
+  } ;
+  /// \brief Workers may optionally broadcast this message to indicate occurrance of certain events
+  ///
+  class worker_status_message : public notification_message {
+    string worker_type_;
+    string worker_id_;
+    worker_status_t status_;
+    string timestamp_;
+  public:
+    worker_status_message( const string& worker_type, const string& worker_id, worker_status_t status );
+    worker_status_message( JSON::Object::Ptr json );
+    virtual ~worker_status_message() {}
+    virtual string to_json() const;
+
+    const string& worker_type() const { return worker_type_; }
+    const string& worker_id() const { return worker_id_; }
+    const worker_status_t status() const { return status_; }
+    const string& timestamp() const { return timestamp_; }
+
+    static const char* NOTIFICATION_TYPE;
+  };
+
+  typedef shared_ptr<worker_status_message> worker_status_message_ptr_t;
+
+}
+
+#endif // worker_status_message_hpp
